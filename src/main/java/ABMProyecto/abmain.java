0a787865086ee2a496dd245d0388b610a68c75b5
adrianbaldonedo <adrianabril1989@gmail.com>
2025-03-08 14:37:39 +0100

    "v_4.2_estilo mejorado"

--- src/main/java/ABMProyecto/abmain.java
+++ src/main/java/ABMProyecto/abmain.java
@@ -19,22 +19,22 @@ public class abmain {
 	public static void main(String[] args) {
 		SpringApplication.run(abmain.class, args);
 	}
-	// @Bean
+	//  @Bean
 	// CommandLineRunner initData(ProductoService productoService,ClienteService clienteservice,ProveedorService proveedorService,TipoService tipoService,UsuarioService usuarioService ){
-	// 	return args -> {
-			// Usuario us1=usuarioService.añadir(
-			// 		new Usuario(1L, "Adrian", "1234", Rol.ADMINISTRADOR));
-			// Usuario us2=usuarioService.añadir(
-			// 		new Usuario(2L, "user", "1234", Rol.USUARIO));
-			// Cliente clien1=clienteservice.añadir(new Cliente(1L,"Patricia"));
-			// Cliente client2=clienteservice.añadir(new Cliente(2L,"Juan"));
-			// Proveedor proveed1=proveedorService.añadir(new Proveedor(1L,"Martinez"));
-			// Proveedor proveed2=proveedorService.añadir(new Proveedor(2L,"Ochoa"));
-			// Tipo tip1=tipoService.añadir(new Tipo(1L,"Pan"));
-			// Tipo tip2=tipoService.añadir(new Tipo(2L,"Pasteles"));
-			// Producto produc1=productoService.añadir(new Producto(1L,"Barra",5,clien1,proveed1,tip1));
-			// Producto produc2=productoService.añadir(new Producto(2L,"Tarta",50,client2,proveed2,tip2));
+	//  	return args -> {
+	// 		Usuario us1=usuarioService.añadir(
+	// 				new Usuario(1L, "Adrian", "1234", Rol.ADMINISTRADOR));
+	// 		Usuario us2=usuarioService.añadir(
+	// 				new Usuario(2L, "user", "1234", Rol.USUARIO));
+	// 		Cliente clien1=clienteservice.añadir(new Cliente(1L,"Patricia"));
+	// 		Cliente client2=clienteservice.añadir(new Cliente(2L,"Juan"));
+	// 		Proveedor proveed1=proveedorService.añadir(new Proveedor(1L,"Martinez"));
+	// 		Proveedor proveed2=proveedorService.añadir(new Proveedor(2L,"Ochoa"));
+	// 		Tipo tip1=tipoService.añadir(new Tipo(1L,"Pan"));
+	// 		Tipo tip2=tipoService.añadir(new Tipo(2L,"Pasteles"));
+	// 		Producto produc1=productoService.añadir(new Producto(1L,"Barra",5,clien1,proveed1,tip1));
+	// 		Producto produc2=productoService.añadir(new Producto(2L,"Tarta",50,client2,proveed2,tip2));
 		};
-// 	}
+//  	}
 
-//  }
+// }
